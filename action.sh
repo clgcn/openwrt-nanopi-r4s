3fc913c80efb83edcd6c86f5be8c96f5d1b8a7e1
Chenglan Guo <clguo@thoughtworks.com>
2024-06-16 17:55:10 +0800

    fix no image error

--- action.sh
+++ action.sh
@@ -6,7 +6,7 @@ function cleanup() {
 		sudo rm -rf /swapfile
 	fi
 	sudo rm -rf /etc/apt/sources.list.d/* /usr/share/dotnet /usr/local/lib/android /opt/ghc
-	command -v docker && docker rmi $(docker images -q)
+	command -v docker && docker rmi $(docker images -q) 2>&1 /dev/null
 	sudo apt-get -y purge \
 		azure-cli* \
 		ghc* \
