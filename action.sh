8df62fd51e726b715d7e4e8e8335f465927afa99
clg <chenglanguo@ritchiebros.com>
2024-12-22 21:08:39 +0800

    fix error

--- action.sh
+++ action.sh
@@ -29,8 +29,7 @@ function init() {
 		sudo apt-get clean all
 	)
 	sudo apt-get update
-	sudo apt-get install make gcc g++ libncurses5-dev unzip -y
-	sudo apt-get -y install subversion build-essential libncurses5-dev zlib1g-dev gawk git ccache gettext libssl-dev xsltproc zip
+	sudo apt-get -y install build-essential clang flex bison g++ gawk gcc-multilib g++-multilib gettext git libncurses-dev libssl-dev python3-distutils python3-setuptools rsync swig unzip zlib1g-dev file wget
 	wget -O- https://raw.githubusercontent.com/friendlyarm/build-env-on-ubuntu-bionic/master/install.sh | bash
 	sudo apt-get autoremove --purge -y
 	sudo apt-get clean
